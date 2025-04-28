b8aee3e2534857b93e6180aef8342dcaff0c8f9f
Jeronimo Capelle <jcapelle@mail.austral.edu.ar>
2025-04-28 11:21:50 -0300

    Finished

--- lists.py
+++ lists.py
@@ -1,20 +1,36 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    if len(list_to_remove_elements) > 5: del list_to_remove_elements[5]
+    if len(list_to_remove_elements) > 4: del list_to_remove_elements[4]
+    if len(list_to_remove_elements) > 0: del list_to_remove_elements[0]
+    
+    return list_to_remove_elements
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    list_to_add_elements.insert(0, 'Pink')
+    list_to_add_elements.insert(len(list_to_add_elements), 'Yellow')
+
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return len(list_to_check) == 0  # Remove this line and implement
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    try:
+        list_to_compare1[2]
+        list_to_compare2[2]
+    except:
+        return False
+    
+    return (list_to_compare1[2] == list_to_compare2[2])
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return [list_of_lists_to_modify[0][:2],list_of_lists_to_modify[1][1:4],list_of_lists_to_modify[2][-2:]]
